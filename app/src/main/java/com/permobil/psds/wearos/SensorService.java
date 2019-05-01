b1b766ae9f99aae6fb5e8cc06cc74be848263f28
Brad Martin <bradwaynemartin@gmail.com>
2019-05-01 11:41:00 -0500

    cleanup old unused code

--- app/src/main/java/com/permobil/psds/wearos/SensorService.java
+++ app/src/main/java/com/permobil/psds/wearos/SensorService.java
@@ -13,7 +13,6 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
@@ -23,7 +22,6 @@ import android.util.Log;
 import androidx.core.app.ActivityCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import com.google.api.client.json.GenericJson;
 import com.kinvey.android.Client;
 import com.kinvey.android.store.DataStore;
 import com.kinvey.android.sync.KinveyPushCallback;
@@ -32,9 +30,6 @@ import com.kinvey.java.KinveyException;
 import com.kinvey.java.core.KinveyClientCallback;
 import com.kinvey.java.store.StoreType;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -42,32 +37,16 @@ public class SensorService extends Service {
 
     private static final String TAG = "PermobilSensorService";
 
-    private Client mKinveyClient;
     private DataStore<PSDSData> psdsDataStore;
     private Handler mHandler;
-    private String sdCardPath;
     private WakeLock mWakeLock;
     private String userIdentifier;
     private String deviceUUID;
-    private FileWriter writer;
-    private SensorManager mSensorManager;
     private LocationManager mLocationManager;
     private Runnable mHandlerTask;
     private SensorEventListener mListener;
 
-//    private String dataFileName = sdCardPath + "/permobil_sensor_data.txt";
-//    private File dataFile = new File(dataFileName);
-
-    private Sensor mLinearAcceleration;
-    private Sensor mGravity;
-    private Sensor mMagneticField;
-    private Sensor mRotationVector;
-    private Sensor mGameRotationVector;
-    private Sensor mGyroscope;
-    private Sensor mProximity;
-    private Sensor mOffBodyDetect;
-
-    public static ArrayList<SensorServiceData> sensorServiceDataList = new ArrayList<>();
+    public static ArrayList<PSDSData.SensorData> sensorServiceDataList = new ArrayList<>();
 
     public SensorService() {
     }
@@ -90,9 +69,8 @@ public class SensorService extends Service {
         );
         this.deviceUUID = uuid;
         this.mHandler = new Handler();
-        this.sdCardPath = Environment.getExternalStorageDirectory().getPath();
 
-        this.mKinveyClient = ((App) getApplication()).getSharedClient();
+        Client mKinveyClient = ((App) getApplication()).getSharedClient();
         Log.d(TAG, "Kinvey Client from App.java found and set in service.");
 
         // Get the Kinvey Data Collection for storing data
@@ -117,20 +95,11 @@ public class SensorService extends Service {
             // check for reporting delay
             int reportingDelay = extras.getInt(Constants.MAX_REPORTING_DELAY, 0);
             maxReportingLatency = reportingDelay != 0 ? reportingDelay : 50000000;
-            String savedStudyId = getSharedPreferences("com.permobil.psds.wearos", Context.MODE_PRIVATE).getString(Constants.SAVED_STUDY_ID, "");
-            userIdentifier = savedStudyId;
+            userIdentifier = getSharedPreferences("com.permobil.psds.wearos", Context.MODE_PRIVATE).getString(Constants.SAVED_STUDY_ID, "");
         } else {
             sensorDelay = SensorManager.SENSOR_DELAY_UI;
             maxReportingLatency = 50000000;
             userIdentifier = null;
-        }
-
-        try {
-            writer = new FileWriter(new File(sdCardPath, "permobil_sensors_" + System.currentTimeMillis() + ".txt"));
-            Log.d(TAG, "New FileWriter: " + writer.toString());
-        } catch (IOException e) {
-            Log.e(TAG, "Error creating new FileWriter: " + e.getMessage());
-            e.printStackTrace();
         }
 
         // Handle wake_lock so data collection can continue even when screen turns off
@@ -143,7 +112,7 @@ public class SensorService extends Service {
         mHandlerTask = new Runnable() {
             @Override
             public void run() {
-                _writeAndUpload();
+                _UploadDataToKinvey();
                 mHandler.postDelayed(mHandlerTask, 60 * 1000);
             }
         };
@@ -153,25 +122,14 @@ public class SensorService extends Service {
         return START_STICKY; // START_STICKY is used for services that are explicitly started and stopped as needed
     }
 
-    private void _writeAndUpload() {
-//        try {
-//            writer.write(String.valueOf(SensorService.sensorServiceDataList));
-//            Log.d(TAG, "Wrote the sensor data to file.");
-//        } catch (IOException e) {
-//            System.out.println("Exception");
-//        }
-        Log.d(TAG, "_writeAndUpload()...");
+    private void _UploadDataToKinvey() {
+        Log.d(TAG, "_UploadDataToKinvey()...");
         PSDSData data = new PSDSData();
-        Log.d(TAG, "new PSDSData created: " + data);
-        data.device_uuid = this.deviceUUID;
-
-        // BRAD - thinking that the GC messages are about the static arraylist and retaining sensor data
-        Log.d(TAG, "ServiceDataList size: " + sensorServiceDataList.size());
-        data.sensor_data = SensorService.sensorServiceDataList;
-        Log.d(TAG, "PSDSData sensor_data values: " + data.sensor_data);
-        SensorService.sensorServiceDataList = new ArrayList<>();
-//        data.sensor_list = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         data.user_identifier = this.userIdentifier;
+        data.device_uuid = this.deviceUUID;
+        data.sensor_data = sensorServiceDataList;
+        // reset the sensor data list for new values to be pushed into
+        SensorService.sensorServiceDataList = new ArrayList<>();
 
         // if we have location permission write the location to record, if not, just print WARNING to LogCat, not sure on best handling for UX right now.
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -207,7 +165,6 @@ public class SensorService extends Service {
                                     ": " + throwable.getMessage());
                             Log.e(TAG, "Kinvey push failure cause: " + throwable.getCause());
                             sendMessageToActivity(throwable.getMessage());
-
                         }
 
                         @Override
@@ -231,16 +188,8 @@ public class SensorService extends Service {
     public void onDestroy() {
         super.onDestroy();
         if (this.mWakeLock != null) {
+            Log.d(TAG, "Releasing wakelock for SensorService.");
             this.mWakeLock.release();
-        }
-        if (writer != null) {
-            try {
-                Log.d(TAG, "Closing the file writer...");
-                writer.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error closing FileWriter: " + e.getMessage());
-                e.printStackTrace();
-            }
         }
 
         // remove handler tasks
@@ -303,45 +252,43 @@ public class SensorService extends Service {
     }
 
     private boolean _registerDeviceSensors(int delay, int reportingLatency) {
-        mSensorManager = (SensorManager) getApplicationContext()
+        SensorManager mSensorManager = (SensorManager) getApplicationContext()
                 .getSystemService(SENSOR_SERVICE);
         // make sure we have the sensor manager for the device
         if (mSensorManager != null) {
-            Log.d(TAG, "Sensor Manager: " + mSensorManager.toString());
-
             Log.d(TAG, "Creating sensor listener...");
             mListener = new SensorListener();
 
             // register all the sensors we want to track data for
-            mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+            Sensor mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
             if (mLinearAcceleration != null)
                 mSensorManager.registerListener(mListener, mLinearAcceleration, delay, reportingLatency);
 
-            mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+            Sensor mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
             if (mGravity != null)
                 mSensorManager.registerListener(mListener, mGravity, SensorManager.SENSOR_DELAY_NORMAL, 50000000);
 
-            mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            Sensor mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
             if (mMagneticField != null)
                 mSensorManager.registerListener(mListener, mMagneticField, delay, reportingLatency);
 
-            mRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            Sensor mRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
             if (mRotationVector != null)
                 mSensorManager.registerListener(mListener, mRotationVector, delay, reportingLatency);
 
-            mGameRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+            Sensor mGameRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
             if (mGameRotationVector != null)
                 mSensorManager.registerListener(mListener, mGameRotationVector, delay, reportingLatency);
 
-            mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            Sensor mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
             if (mGyroscope != null)
                 mSensorManager.registerListener(mListener, mGyroscope, delay, reportingLatency);
 
-            mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            Sensor mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             if (mProximity != null)
                 mSensorManager.registerListener(mListener, mProximity, delay, reportingLatency);
 
-            mOffBodyDetect = mSensorManager.getDefaultSensor(Sensor.TYPE_LOW_LATENCY_OFFBODY_DETECT);
+            Sensor mOffBodyDetect = mSensorManager.getDefaultSensor(Sensor.TYPE_LOW_LATENCY_OFFBODY_DETECT);
             if (mOffBodyDetect != null)
                 mSensorManager.registerListener(mListener, mOffBodyDetect, delay, reportingLatency);
         } else {
