fa8d5bd319995c8e14f5de438c97b7feb604b961
William Emfinger <waemfinger@gmail.com>
2019-05-20 10:58:55 -0500

    updated timing for sending records since they should be faster now.

--- app/src/main/java/com/permobil/psds/wearos/SensorService.java
+++ app/src/main/java/com/permobil/psds/wearos/SensorService.java
@@ -61,8 +61,8 @@ public class SensorService extends Service {
     private static final int sensorDelay = android.hardware.SensorManager.SENSOR_DELAY_UI; // microseconds between sensor data // 60 * 1000;
     private static final int maxReportingLatency = 15 * 1000 * 1000; // 15 seconds between sensor updates in microseconds
     // TODO: change these values for release
-    private static final int SAVE_TASK_PERIOD_MS = 1 * 60 * 1000;
-    private static final int SEND_TASK_PERIOD_MS = 1 * 60 * 1000;
+    private static final int SAVE_TASK_PERIOD_MS = 1 * 60 * 1000; // each record will be 1 minute long
+    private static final int SEND_TASK_PERIOD_MS = 10 * 1000; // send a record every 10 seconds if possible
     private static final long LOCATION_LISTENER_MIN_TIME_MS = 5 * 60 * 1000;
     private static final float LOCATION_LISTENER_MIN_DISTANCE_M = 100;
     private static final int MAX_SEND_COUNT = 1;
