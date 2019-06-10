5da5d12042d1c71b1189b0bf19f069b16781fdc5
William Emfinger <waemfinger@gmail.com>
2019-06-10 08:54:34 -0500

    decrease sampling rate of sensors and incerase reporting latency to improve battery life.

--- app/src/main/java/com/permobil/psds/wearos/SensorService.java
+++ app/src/main/java/com/permobil/psds/wearos/SensorService.java
@@ -59,14 +59,14 @@ public class SensorService extends Service {
     private static final int NOTIFICATION_ID = 543;
     private static final int NETWORK_CONNECTIVITY_TIMEOUT_MS = 60000;
     private static final int SENSOR_DELAY_DEBUG = 40 * 1000; // microseconds between sensor data
-    private static final int SENSOR_DELAY_RELEASE = 100 * 1000;// microseconds between sensor data
-    private static final int maxReportingLatency = 60 * 1000 * 1000; // 60 seconds between sensor updates in microseconds
+    private static final int SENSOR_DELAY_RELEASE = SensorManager.SENSOR_DELAY_NORMAL; // approx 200 ms between sensor data
+    private static final int maxReportingLatency = 3 * 60 * 1000 * 1000; // 3 minutes between sensor updates in microseconds
     private static final int SAVE_TASK_PERIOD_MS = 60 * 1000; // each record will be 1 minute long
     private static final int SEND_TASK_PERIOD_MS = 10 * 1000; // send a record every 10 seconds if possible
     private static final long LOCATION_LISTENER_MIN_TIME_MS = 5 * 60 * 1000;
     private static final float LOCATION_LISTENER_MIN_DISTANCE_M = 100;
     private static final int MAX_SEND_COUNT = 1;
-    private static final int MAX_NUM_ENTRIES_PER_RECORD = 5 * 10 * 60 * 5; // 5 sensors * 10 Hz * 60 seconds * 5 minutes = max length of each log
+    private static final int MAX_NUM_ENTRIES_PER_RECORD = 5 * 5 * 60 * 5; // 5 sensors * 5 Hz * 60 seconds * 5 minutes = max length of each log
 
     private String userIdentifier;
     private String deviceUUID;
